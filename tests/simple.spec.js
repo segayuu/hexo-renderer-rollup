ed72653760f048f0f844478188f0c26fa48e620d
segayuu <segayuu@gmail.com>
2018-06-25 13:14:10 +0900

    [WIP] renderer simple implement

---
+++ tests/simple.spec.js
@@ -0,0 +1,42 @@
+'use strict';
+const Hexo = require('hexo');
+const { createSandbox, process } = require('hexo-test-utils/core');
+
+const { join } = require('path');
+const fixture_folder = join(__dirname, 'fixtures');
+
+/**
+ * @param {string} path
+ * @param {Hexo} ctx
+ */
+const jsRender = (path, ctx) => ctx.render.render({ path });
+
+/** @type {(fixture?: string) => Promise<Hexo>} */
+const sandbox = createSandbox(Hexo, { fixture_folder, plugins: [require.resolve('../lib')] });
+
+test('through', async () => {
+  const fixtureName = 'simple';
+  const fixturePath = join(fixture_folder, fixtureName);
+  const hexo = await sandbox(fixtureName);
+  await process(hexo);
+
+  const result = await jsRender(join(fixturePath, 'app.js'), hexo);
+  expect(typeof result).toBe('string');
+});
+
+test('iife', async () => {
+  const fixtureName = 'simple';
+  const fixturePath = join(fixture_folder, fixtureName);
+  const hexo = await sandbox(fixtureName);
+
+  hexo.config.rollup.output = {
+    file: 'bundle.js',
+    format: 'iife',
+    name: 'hexoRollup'
+  };
+
+  await process(hexo);
+
+  const result = await jsRender(join(fixturePath, 'app.js'), hexo);
+  expect(typeof result).toBe('string');
+});
